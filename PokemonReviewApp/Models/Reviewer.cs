f7ccd1aa1f38fe8d521454d00fa037a5bb53b10d
SyedMuhammadHaris <mohammadharisahmed.18@gmail.com>
2023-02-20 01:06:25 +0500

    Add project files.

---
+++ PokemonReviewApp/Models/Reviewer.cs
@@ -0,0 +1,12 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Reviewer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string email { get; set; }
+
+
+        public ICollection<Review> Reviews { get; set; }
+    }
+}
