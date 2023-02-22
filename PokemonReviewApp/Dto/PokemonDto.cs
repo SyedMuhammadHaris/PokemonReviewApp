070eb162eb5ea1e3f0d2e755675dda1b48a6704f
SyedMuhammadHaris <mohammadharisahmed.18@gmail.com>
2023-02-23 00:08:43 +0500

    Get Methods (Get All POkemons and Get Pokemon By Id)

---
+++ PokemonReviewApp/Dto/PokemonDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonReviewApp.Dto
+{
+    // In dto we limit the we exposed to api
+    public class PokemonDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
